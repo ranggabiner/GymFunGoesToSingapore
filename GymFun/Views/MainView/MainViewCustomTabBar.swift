ccc9f61c53b7c9afe097b98dbeafeaeb0ee5c77e
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-05-08 15:55:58 +0700

    gymfun v1

---
+++ GymFun/Views/MainView/MainViewCustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewCustomTabBar.swift
+//  GymFun
+//
+//  Created by Rangga Biner on 08/05/24.
+//
+
+import SwiftUI
+
+struct MainViewCustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewCustomTabBar()
+}
